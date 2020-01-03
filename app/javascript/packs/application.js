474d7133c79fdf9d0457f7891117c984dc977ea6
Mario Lotz <mario@hvboom.ch>
2020-01-03 22:31:03 +0100

    Basic bootstrap setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import('@rails/ujs').start()
+import('turbolinks').start()
+import('@rails/activestorage').start()
+import 'channels'
 
+// Import application specific stylesheets
+import '../stylesheets/application'
+
+// Activate bootstrap
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import '../bootstrap_custom'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
