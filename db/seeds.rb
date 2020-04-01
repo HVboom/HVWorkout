8ec163fb01df5b9ebee2cd30241ac3e45b7e5dbc
Mario Lotz <mario@hvboom.ch>
2020-04-01 21:25:04 +0200

    Add authentication with Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+unless Rails.env.production?
+  # create Demo users
+  3.times do |n|
+    nickname = "Demo#{n+1}"
+    unless User.find_by_nickname(nickname)
+      User.create!(nickname: nickname, email: "#{nickname}@HVboom.ch", password: 'Demo2020', password_confirmation: 'Demo2020', confirmed_at: DateTime.now)
+      puts "User '#{nickname}' created"
+    end
+  end
+end
+
